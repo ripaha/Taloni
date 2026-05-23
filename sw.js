8f8919a7ab023392f1a001992c365f573104ee7b
ripaha <risto.haukipuro@gmail.com>
2026-05-23 03:11:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Tämä tiedosto pyörii selaimessa taustalla ja hallinnoi välimuistia.
 // Aina kun sovellus päivitetään, vaihda CACHE_VERSION numero korkeammaksi.
 
-const CACHE_VERSION = 'v0.9.1';
+const CACHE_VERSION = 'v0.9.2';
 const CACHE_NAME = `taloni-${CACHE_VERSION}`;
 
 // Tiedostot joita välimuistiin tallennetaan asennuksen yhteydessä.
